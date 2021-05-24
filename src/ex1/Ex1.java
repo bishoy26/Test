fa161e50e9f671e053c2f3d612351051b8181cbd
bisho <bisho@192.168.2.10>
2021-05-24 18:55:15 -0400

    initial commit

---
+++ src/ex1/Ex1.java
@@ -0,0 +1,18 @@
+package ex1;
+
+/**
+ * This class +++Inserct Description Here+++
+ *
+ * @author Bishoy
+ */
+public class Ex1
+{
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      System.out.println("Hello World");
+   }
+}
